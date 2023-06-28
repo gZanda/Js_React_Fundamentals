52639e32f8aa0d9889476cd7a5bb998403227d67
gZanda <gabrielfzanda@gmail.com>
2023-06-28 19:57:13 -0300

    Att

--- basic_react/src/App.js
+++ basic_react/src/App.js
@@ -4,7 +4,7 @@
 
 import './App.css';
 import Greeting from './component/Greeting.js'
-import StateHook from './component/StateHook';
+import StateHook from './component/UseStateHook';
 import Home from './component/Home';
 import Home2 from './component/Home2';
 import {BrowserRouter, Route, Routes } from 'react-router-dom';
