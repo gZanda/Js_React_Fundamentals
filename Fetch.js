12c8504f20841c4ce620f07d4132097d4fffefb1
gZanda <gabrielfzanda@gmail.com>
2023-06-28 22:21:02 -0300

    Test API

--- Fetch.js
+++ Fetch.js
@@ -2,7 +2,8 @@
 
 async function fetchData() {
     try {
-      const response = await fetch('http://localhost:8080/notes');
+      // Fetch para uma API teste
+      const response = await fetch('https://jsonplaceholder.typicode.com/comments');
       if (!response.ok) {
         throw new Error('Network response was not OK');
       }
