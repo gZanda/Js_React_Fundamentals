cd83833cd9c6a62adcb6c35aea0e7bf495aadee3
gZanda <gabrielfzanda@gmail.com>
2023-10-01 21:37:31 -0300

    Function adjust

--- basic_react/src/component/UseStateHook.js
+++ basic_react/src/component/UseStateHook.js
@@ -3,42 +3,40 @@ import { useState } from "react";
 
 // UseState - Uma forma de modificar dinamicamente elementos no HTML sem precisar de seletores JS
 
-const StateHook = () =>{
-
+function StateHook() {
     // UseState Variable ( instead of: let counter = 0 )
     // Usa "setCounter" para alterar seu valor
     // useState = Inicial
     // Variáveis com USESTATE e SET
-    let [counter,setCounter] = useState(0);
+    let [counter, setCounter] = useState(0);
     let [userInput, setUserInput] = useState("Input");
 
     // Função de Alterar Input
     // event já carrega automaticamente um input (pois é da TAG e do Event "onChange")
     const alter = (event) => {
         const newValue = event.target.value;
-        setUserInput(newValue); 
-    }
+        setUserInput(newValue);
+    };
 
     // Função de Incrementar um
-    const plusOne = () =>{
-        setCounter(counter + 1 )
-    }
+    const plusOne = (event) => {
+        setCounter(counter + 1);
+    };
 
     // Eventos "onClick" e "onChange" dão trigger nas funções acima
     // No REACT é normal fazer os eventos chamarem as funções diretamente no Front com JSX
     return (
         <div>
- 
-            {counter} 
+            {counter}
             <br></br>
             <br></br>
-            <button onClick={plusOne} > INCREMENT :3 </button>
+            <button onClick={plusOne}> INCREMENT :3 </button>
             <br></br>
             <br></br>
             {userInput}
             <br></br>
             <br></br>
-            <input placeholder="Enter..." onChange={alter} /> 
+            <input placeholder="Enter..." onChange={alter} />
         </div>
     );
 }
